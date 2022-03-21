800e74c4fa5ca44673e229f9c94a1eddf241000c
talles2300 <talles_ramos2012@hotmail.com>
2022-03-21 17:40:15 -0300

    segundo commit

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ var novaDiv = document.querySelector("#novaMensagem");
 button.onclick = function enviar() {
   pergunta1 = document.querySelector("#name");
 
-  pergunta1.appendChild(novaDiv);
+  alert(pergunta1.value);
+
+  document.appendChild("div");
 };
+
+document.addEventListener("keydown", function enviar_enter(e) {
+  if (e.key === "Enter") {
+    pergunta1 = document.querySelector("#name");
+
+    alert(pergunta1.value);
+  }
+});
