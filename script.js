74716fb98512d60d7077527b19981f5e00721cf4
talles2300 <talles_ramos2012@hotmail.com>
2022-03-25 17:45:12 -0300

    sextou

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 var button = document.querySelector("button");
 
+var textBox = document.querySelector("#name");
+
 var pergunta1 = "";
-function enviar_click() {
-  button.onclick = function enviar() {
-    pergunta1 = document.querySelector("#name");
 
-    console.log(pergunta1.value);
+button.onclick = function enviar() {
+  pergunta1 = textBox;
+
+  console.log(pergunta1.value);
+  document.write(pergunta1.value);
+  pergunta1.value = "";
+};
+
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    button.click();
+    pergunta1 = textBox;
+
+    let caixa = document.getElementById("#caixaTexto");
+    let mensagem1 = document.createElement("div");
+    caixa.appendChild(mensagem1);
+
     pergunta1.value = "";
-  };
-}
-
-document.addEventListener("keydown", enviar_click());
+  }
+});
